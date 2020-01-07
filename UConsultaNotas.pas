544b5be164524e083019ad3b465296578f5b4d60
cleomarkfp <45854561+cleomarkfp@users.noreply.github.com>
2020-01-07 18:40:02 -0300

    Incluido novo CPF

--- UConsultaNotas.pas
+++ UConsultaNotas.pas
@@ -90,7 +90,7 @@ begin
   if (vAux <> '40372766072') and (vAux <> '94705836049') and (vAux <> '25527096053')
   and (vAux <> '47698594068') and (vAux <> '38938588068') and (vAux <> '81428650091')
   and (vAux <> '40941345068') and (vAux <> '25024337034') and (vAux <> '99310031620')
-  and (vAux <> '61670812049') then
+  and (vAux <> '61670812049') and (vAux <> '39379230044') then
   begin
     ShowMessage('CPF não entrontrado, favor entrar em contato com a Servisoft pelo fone 51-3598-6584!');
     exit;
@@ -349,7 +349,7 @@ begin
   if (vAux <> '40372766072') and (vAux <> '94705836049') and (vAux <> '25527096053')
    and (vAux <> '47698594068') and (vAux <> '38938588068') and (vAux <> '81428650091')
    and (vAux <> '40941345068') and (vAux <> '25024337034') and (vAux <> '99310031620')
-   and (vAux <> '61670812049') then
+   and (vAux <> '61670812049') and (vAux <> '39379230044') then
   begin
     ShowMessage('CPF não encontrado, favor entrar em contato com a Servisoft pelo fone 51-3598-6584!');
     edtCNPJContabil.Clear;
