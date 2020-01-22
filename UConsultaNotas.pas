d0b4449aa8f26e278115bbee3dfbec04c45137ba
cleomarkfp <45854561+cleomarkfp@users.noreply.github.com>
2020-01-22 17:32:48 -0300

    CPF novo

--- UConsultaNotas.pas
+++ UConsultaNotas.pas
@@ -94,7 +94,8 @@ begin
    and (vAux <> '20584067020') and (vAux <> '58575375091') and (vAux <> '29712165000')
    and (vAux <> '26873010006') and (vAux <> '94247358049') and (vAux <> '40136051049')
    and (vAux <> '39206041053') and (vAux <> '54560306087') and (vAux <> '45154236068')
-   and (vAux <> '25726900049') and (vAux <> '33845999004') and (vAux <> '33844178015') then
+   and (vAux <> '25726900049') and (vAux <> '33845999004') and (vAux <> '33844178015')
+   and (vAux <> '38938588068') then   
   begin
     ShowMessage('CPF não entrontrado, favor entrar em contato com a Servisoft pelo fone 51-3598-6584!');
     exit;
@@ -359,7 +360,8 @@ begin
    and (vAux <> '20584067020') and (vAux <> '58575375091') and (vAux <> '29712165000')
    and (vAux <> '26873010006') and (vAux <> '94247358049') and (vAux <> '40136051049')
    and (vAux <> '39206041053') and (vAux <> '54560306087') and (vAux <> '45154236068')
-   and (vAux <> '25726900049') and (vAux <> '33845999004') and (vAux <> '33844178015') then
+   and (vAux <> '25726900049') and (vAux <> '33845999004') and (vAux <> '33844178015')
+   and (vAux <> '38938588068') then
   begin
     ShowMessage('CPF não encontrado, favor entrar em contato com a Servisoft pelo fone 51-3598-6584!');
     edtCNPJContabil.Clear;
